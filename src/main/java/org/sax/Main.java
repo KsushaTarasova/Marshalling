f1b9070395ae5a80ce056573180588034f6caea4
KsushaTarasova <ksutaras543@mail.ru>
2024-09-24 00:21:56 +0300

    Update Main class

--- src/main/java/org/sax/Main.java
+++ src/main/java/org/sax/Main.java
@@ -1,7 +1,29 @@
 package org.sax;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String path = new File("D:\\BSUIR\\5 sem\\СТРweb-пр\\Marshalling\\src\\main\\resources\\books.xml")
+                .getAbsolutePath();
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            BookXMLHandler handler = new BookXMLHandler();
+            parser.parse(new File(path), handler);
+        } catch (IOException x) {
+            System.err.println(x);
+        } catch (ParserConfigurationException e) {
+            System.err.println(e.getMessage());
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
 
     }
 }
