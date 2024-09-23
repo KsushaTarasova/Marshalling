205d9be62e6b6237ee622f36a00ddd17c594fe5a
KsushaTarasova <ksutaras543@mail.ru>
2024-09-24 00:23:47 +0300

    Update Catalog class

--- src/main/java/org/sax/Catalog.java
+++ src/main/java/org/sax/Catalog.java
@@ -1,4 +1,29 @@
 package org.sax;
 
+import java.util.ArrayList;
+
 public class Catalog {
+    private ArrayList<Book> books;
+
+    public Catalog() {
+        books = new ArrayList<>();
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Book b : books) {
+            sb.append(b.toString());
+        }
+        return sb.toString();
+    }
+
 }
